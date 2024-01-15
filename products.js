f3ae6de9fdb451386648aa29bc244563f46a1066
chetan195 <102486446+chetan195@users.noreply.github.com>
2024-01-15 17:59:36 +0530

    Product Lists

---
+++ products.js
@@ -0,0 +1,7 @@
+const productList = [
+    {id: 1, name:   "Cable"   , price:100},
+    {id: 2, name: "Phone Case", price:150},
+    {id: 3, name: "C Type USB", price:300},
+    {id: 4, name: "Trimmer"   , price:750},
+    
+];
